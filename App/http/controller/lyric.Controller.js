ce9082255c6340f89621bdfb92035ff008f440d4
mohammadj789 <mohammadsoltanian10@gmail.com>
2023-11-11 21:54:04 +0330

    new lyric

--- App/http/controller/lyric.Controller.js
+++ App/http/controller/lyric.Controller.js
@@ -3,12 +3,34 @@ const createHttpError = require("http-errors");
 const { Controller } = require("./Controller");
 const Lyric = require("../../models/lyric");
 const { songSchema } = require("../validation/lyric.validation");
+const Song = require("../../models/song");
 
 class LyricController extends Controller {
   async newLyric(req, res, next) {
     try {
       const user = req.user;
       await songSchema.validateAsync(req.body);
+
+      const track = await Song.findById(req.body.track);
+      // const prevlyric = await Lyric.findOne({});
+      if (!track)
+        throw createHttpError.NotFound(
+          "there is no track to get lyric"
+        );
+      if (track.lyric)
+        throw createHttpError.BadRequest(
+          "this track already has a valid lyric"
+        );
+      const prevlyric = await Lyric.findOne({
+        track: track._id,
+        "writer.writer_id": user._id,
+      });
+      if (prevlyric?.status === "pending")
+        throw createHttpError.BadRequest(
+          "you have submitted your request once.wait"
+        );
+      else if (prevlyric?.status === "rejected")
+        throw createHttpError.BadRequest("your request is rejected");
 
       const lyric = await Lyric.create({
         lyric: req.body.lyric,
@@ -19,7 +41,13 @@ class LyricController extends Controller {
         },
       });
       if (!lyric) throw createHttpError.InternalServerError();
-      res.status(201).send(lyric);
+      res
+        .status(201)
+        .send({
+          status: 200,
+          message: 'we"ll respond to your request ASAP',
+          lyric,
+        });
     } catch (error) {
       next(error);
     }
--- App/routers/lyric.routes.js
+++ App/routers/lyric.routes.js
@@ -4,8 +4,9 @@ const { checkAuth } = require("../http/middleware/checkAuth");
 const {
   LyricController,
 } = require("../http/controller/lyric.Controller");
+const { stringToArray } = require("../http/middleware/stringToArray");
 
 const router = Router();
 
-router.post("/new", LyricController.newLyric);
+router.post("/new", checkAuth, LyricController.newLyric);
 module.exports = { LyricRoutes: router };
