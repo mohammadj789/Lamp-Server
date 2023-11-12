6bc758c01b8341b3001addfddee645378020b9ca
mohammadj789 <mohammadsoltanian10@gmail.com>
2023-11-12 18:44:52 +0330

    port for liara

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require("dotenv").config();
 const { Application } = require("./App/app");
 const PORT = process.env.PORT || 3030;
 const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/Lamp";
-new Application(4000, DB_URL);
+new Application(PORT, DB_URL);
