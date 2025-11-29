1dd0bcfa7b61325115bd45e2fe812b9bad1b4b99
mohammadjavad <mohammadsoltanian10@gmail.com>
2025-11-29 13:22:25 +0330

    index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("express");
 const { Application } = require("./App/app");
 const PORT = process.env.PORT || 4000;
 const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/Lamp";
